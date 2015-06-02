6d553f3ca0cf237e56e4743c20be26168a8d0d94
nihiven <jb@nihiven.net>
2015-06-02 12:05:06 -0400

    basic lists

--- ntunes.js
+++ ntunes.js
@@ -1,23 +1,130 @@
+///// aldeed:autoform
+
+//////////// Global stuff? is that right?
+Lists = new Mongo.Collection("lists");
+Files = new Mongo.Collection("files");
+Favorites = new Mongo.Collection("favorites");
+
+
+//////////// client stuff
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    files: function () {
+      return Files.find({}, {sort: {name: 1}});
+    },
+    notMylists: function () {
+      return Lists.find({ownerId: {$ne: Meteor.userId()}}, {sort: {name: 1}});
+    },
+    myLists: function () {
+      return Lists.find({ownerId: Meteor.userId()}, {sort: {name: 1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-file": function (event) {
+      // This function is called when the new task form is submitted
+      var fileName = event.target.fileName.value;
+
+      // do stuff here dog
+
+      Files.insert({
+        name: fileName,
+        ownerId: Meteor.userId(),
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.fileName.value = "";
+
+      // Prevent default form submit
+      return false;
+    },
+
+    "click .upload": function (event) {
+      // This function is called when the new task form is submitted
+      var fileName = event.target.fileName.value;
+
+      // do stuff here dog
+
+      Files.insert({
+        name: fileName,
+        ownerId: Meteor.userId(),
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.fileName.value = "";
+
+      // Prevent default form submit
+      return false;
+    },
+
+    "submit .new-list": function (event) {
+      // This function is called when the new task form is submitted
+      var listName = event.target.listName.value;
+
+      // do stuff here dog
+
+      Lists.insert({
+        name: listName,
+        ownerId: Meteor.userId(),
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.listName.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
+
+  Template.file.events({
+    "click .favorite": function () {
+      // search for file id in user's favorites
+      var index = Meteor.user().profile.favorites.indexOf(this._id);
+      if (index > -1) {
+        // remove the file id if found
+        Meteor.users.update(Meteor.userId(), {$pull: {"profile.favorites": this._id}}, {multi: true});
+      } else {
+        // add the file id if not found
+        Meteor.users.update(Meteor.userId(), {$addToSet: {"profile.favorites": this._id}});  
+      }
+    }
+  });
+
+  Template.myList.events({
+    "click .delete": function () {
+      // TODO: confirm
+      Lists.remove(this._id);
+    }
+  });
+
+  Template.file.helpers({
+    "isFavorite": function () {
+      // search for file in in user's favorites
+      var index = Meteor.user().profile.favorites.indexOf(this._id);
+
+      // return a bootstrap class for row highlighting
+      // TODO: replace the info css. is this right? setting style in the js?
+      return ((index > -1) ? "fa-heart" : "fa-heart-o");
+    }
+  });
+
 }
 
+
+//////////// server stuff
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    
   });
 }
+
+
+/*
+schema for lists
+
+
+
+*/
